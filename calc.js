dd7734fe0219099dadfc671ef483b1a69ad26de2
aclar8709 <pythontest8709@gmail.com>
2025-03-15 08:16:33 -0400

    added calc

---
+++ calc.js
@@ -0,0 +1,74 @@
+// ****** SELECT ITEMS *****
+const entry = document.getElementById("entry");
+const calc = document.getElementById("calc-body");
+
+let operation = ''; 
+let operationList = ['+','-','*','/','%']
+// ****** EVENTLISTENERS *****
+calc.addEventListener("submit", computeValue)
+
+// ****** FUNCTIONS *****
+function computeValue(e)
+{
+    e.preventDefault();
+    console.log(entry.value);
+
+    let entryString = entry.value;
+    let x = '';
+    let y = '';
+    let switchNum = false;
+    for(let digit of entryString)
+    {
+        if(operationList.includes(digit))
+        {
+            switchNum = true;
+        }
+        else
+        {
+            if(switchNum)
+            {
+                y += digit;
+            }
+            else
+            {
+                x += digit;
+            }
+        }
+    }
+    calculate(operation,parseInt(x),parseInt(y));
+}
+function clearEntry()
+{
+    entry.value = '';
+    operation = '';
+}
+function appendNum(digit)
+{
+    entry.value += digit;
+}
+function appendOp(text, op)
+{
+    entry.value += text;
+    operation = op;
+}
+function calculate(op, x, y)
+{
+    switch(op){
+        case 'add':
+            appendNum(' = ' + (x+y));
+            break;
+        case 'subtract':
+            appendNum(' = ' + (x-y));
+            break;
+        case 'multiply':
+            appendNum(' = ' + (x*y));
+            break;
+        case 'divide':
+            appendNum(' = ' + (x/y));
+            break;
+        case 'mod':
+            console.log("To be implemented");
+            break;
+    }
+
+}
